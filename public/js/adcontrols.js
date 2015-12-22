63f972398bd61f8a6e21489f314ef09122eb342e
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-12-22 16:08:51 -0200

    adicionado controles

---
+++ public/js/adcontrols.js
@@ -0,0 +1,31 @@
+/**
+ * GA
+*/
+var _gaq = _gaq || [];
+_gaq.push(
+    ['b._setAccount', 'UA-12413371-1'],
+    ['b._trackPageview'],
+    ['b._setDomainName', 'gazetaonline.com.br'],
+    ['c._setAccount', 'UA-1316029-3'],
+    ['c._trackPageview'],
+    ['d._setAccount', 'UA-59638019-1'],
+    ['d._trackPageview'],
+    ['d._setDomainName', 'noticiaagora.com.br']
+);
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+var _vrq = _vrq || [];
+_vrq.push(['id', 244]);
+_vrq.push(['automate', true]);
+_vrq.push(['track', function(){}]);
+(function(d, a){
+    var s = d.createElement(a),
+        x = d.getElementsByTagName(a)[0];
+    s.async = true;
+    s.src = 'http://a.visualrevenue.com/vrs.js';
+    x.parentNode.insertBefore(s, x);
+})(document, 'script');
+
