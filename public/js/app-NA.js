ede2097275a12c8aa6f021a524c5fd1af53935ac
David <davidpvilaca@outlook.com>
2015-12-22 15:23:35 -0200

    v1.1

--- public/js/app-NA.js
+++ public/js/app-NA.js
@@ -5,23 +5,43 @@
     
     function home($scope, $timeout, loading, $resource){
         /* jshint validthis: true */
-        var vm = this;
+        var vm = this, c=0;
         
         vm.content = [];
         
         loading.start();
         
         
-        var contentPromise = $resource('/rest/noticias').get().$promise;
+        var content1Promise = $resource('/rest/noticias').get().$promise,
+            content2Promise = $resource('/rest/noticias').get().$promise,
+            content3Promise = $resource('/rest/noticias').get().$promise,
+            content4Promise = $resource('/rest/noticias').get().$promise;
         
-        contentPromise
+        content1Promise
+        .then(successContent)
+        .catch(failPromise);
+        
+        content2Promise
+        .then(successContent)
+        .catch(failPromise);
+        
+        content3Promise
+        .then(successContent)
+        .catch(failPromise);
+        
+        content4Promise
         .then(successContent)
         .catch(failPromise);
         
         function successContent(data){
-            vm.content = data.items;
-            //todo renderizar o objeto
-            loading.complete();
+            vm.content.push(data.items.filter(function(el){
+                return el.dominantthumbnail && el.title && el.click_url;
+            }));
+            if(c === 3){
+                loading.complete();
+            }else{
+                c++;
+            }
         }
         
         function failPromise(err){
