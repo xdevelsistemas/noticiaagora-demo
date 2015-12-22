6e05f72ffe4d2cbfbc90840c156b35723742e10f
David <davidpvilaca@outlook.com>
2015-12-22 17:54:09 -0200

    iniciando filtro

--- public/js/app-NA.js
+++ public/js/app-NA.js
@@ -4,18 +4,34 @@
     .controller('home', ['$scope', '$timeout', 'loading', '$resource', home]);
     
     function home($scope, $timeout, loading, $resource){
+        loading.start();
+        
         /* jshint validthis: true */
         var vm = this, c=0;
         
         vm.content = [];
+        vm.categorias = [
+            {"id": 0, "name": "TODAS AS CATEGORIAS"},
+            {"id": 1, "name": "ENTRETENIMENTO"},
+            {"id": 2, "name": "ESPORTES"},
+            {"id": 3, "name": "NOTÍCIAS"}
+        ];
+        vm.filter1 = [
+            {"id": 0, "name": "+RECENTES"},
+            {"id": 1, "name": "+ACESSADAS"}
+        ];
+        vm._model = {
+            categoria: {"id": 0, "name": "TODAS AS CATEGORIAS"},
+            filter: {"id": 0, "name": "+RECENTES"}
+        };
         
-        loading.start();
+        vm.filtrar = filtrar;
         
         
-        var content1Promise = $resource('/rest/noticias').get().$promise,
-            content2Promise = $resource('/rest/noticias').get().$promise,
-            content3Promise = $resource('/rest/noticias').get().$promise,
-            content4Promise = $resource('/rest/noticias').get().$promise;
+        var content1Promise = $resource('/rest/noticias').query().$promise,
+            content2Promise = $resource('/rest/noticias').query().$promise,
+            content3Promise = $resource('/rest/noticias').query().$promise,
+            content4Promise = $resource('/rest/noticias').query().$promise;
         
         content1Promise
         .then(successContent)
@@ -42,6 +58,10 @@
         
         function failPromise(err){
             console.log(err);
+        }
+        
+        function filtrar(){
+            $resource('/rest/noticias')
         }
         
     }
